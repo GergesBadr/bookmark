d14a31ed686e860f3a8ab3fa510545cd691cedbd
GergesBadr <gergesbadr77@gmail.com>
2023-06-18 00:47:29 +0300

    edited files path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./docs/**/*.{html,js}"],
+  content: ["**/*.{html,js}"],
   theme: {
     extend: {},
   },
